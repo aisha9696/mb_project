dfa608f17bb0f675243de37f1e02e70297a8a1df
aishaabylgazy <aishaabylgazy1996@gmail.com>
2024-01-31 23:25:50 +0600

    fixup! first transaction edited

--- src/main/java/kz/mb/project/mb_project/service/NotificationSender.java
+++ src/main/java/kz/mb/project/mb_project/service/NotificationSender.java
@@ -1,7 +1,5 @@
 package kz.mb.project.mb_project.service;
 
-import jakarta.mail.MessagingException;
-
 @FunctionalInterface
 public interface NotificationSender {
   void sendNotification(String to, String subject, String message) ;
